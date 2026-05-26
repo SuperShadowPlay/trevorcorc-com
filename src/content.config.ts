38e44d87098c49164cee7a695cb7abdca8aceb07
SuperShadowPlay <tj@trevorcorc.com>
2026-05-26 18:51:09 -0400

    Portfolio Index Page

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,6 +6,19 @@ const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
+	schema: () =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			// Transform string to Date object
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+		}),
+});
+
+const portfolio = defineCollection({
+	loader: glob({ base: './src/content/portfolio', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
 	schema: ({ image }) =>
 		z.object({
 			title: z.string(),
@@ -17,4 +30,4 @@ const blog = defineCollection({
 		}),
 });
 
-export const collections = { blog };
+export const collections = { blog, portfolio };
